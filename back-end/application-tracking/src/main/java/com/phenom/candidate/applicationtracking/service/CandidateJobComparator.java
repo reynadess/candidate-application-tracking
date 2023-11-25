0b5c23ef2b6c0c639da43bea9a2eeabdf12dfd37
Sai Chaitanya Raparti <chaitanya.raparti@ezetap.com>
2023-11-25 15:25:44 +0530

    Candidate Job Comparator

---
+++ back-end/application-tracking/src/main/java/com/phenom/candidate/applicationtracking/service/CandidateJobComparator.java
@@ -0,0 +1,38 @@
+package com.phenom.candidate.applicationtracking.service;
+
+import com.phenom.candidate.applicationtracking.entities.Candidate;
+import com.phenom.candidate.applicationtracking.entities.Job;
+import java.util.List;
+
+public class CandidateJobComparator {
+
+
+  public static int compare(Candidate candidate, Job j1, Job j2) {
+    List<String> candidateSkills = candidate.getSkillset();
+    List<String> job1Skills = j1.getSkillsRequired();
+    List<String> job2Skills = j2.getSkillsRequired();
+    // Candidates best job fit
+    int job1FitScore = 0;
+    int job2FitScore = 0;
+    int currIdx = 0;
+    for (int i = job1Skills.size() - 1; i >= 0; i--) {
+      if (candidateSkills.contains(job1Skills.get(i))) {
+        job1FitScore = (2 ^ currIdx);
+      }
+      currIdx += 1;
+    }
+
+    for (int i = job2Skills.size() - 1; i >= 0; i--) {
+      if (candidateSkills.contains(job2Skills.get(i))) {
+        job2FitScore = (2 ^ currIdx);
+      }
+      currIdx += 1;
+    }
+
+    if (job1FitScore < job2FitScore) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+}
