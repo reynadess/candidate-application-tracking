494cd9afa8d4a7a589aa7ae19521c564caa60055
Tagore Hari Ravindra Bollu <tagorehyd@live.co.uk>
2023-11-25 16:23:20 +0530

    Controller Changes

--- back-end/application-tracking/src/main/java/com/phenom/candidate/applicationtracking/controller/TrackingController.java
+++ back-end/application-tracking/src/main/java/com/phenom/candidate/applicationtracking/controller/TrackingController.java
@@ -6,6 +6,7 @@ import com.phenom.candidate.applicationtracking.entities.Job;
 import com.phenom.candidate.applicationtracking.entities.JobApplication;
 import com.phenom.candidate.applicationtracking.entities.Recruiter;
 import com.phenom.candidate.applicationtracking.service.JobRanking;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,48 +16,43 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/searches")
+@CrossOrigin
 public class TrackingController {
 
-	@GetMapping("/getTopJobsForCandidate")
-	public List<Job> getTopJobs(){
+	@GetMapping("/getTopJobsForCandidate") public List<Job> getTopJobs() {
 		Candidate candidate = JobsDao.getAllCandidates().getFirst();
 		List<Job> jobs = JobRanking.getJobsBasedOnCandidateSkills(candidate);
 		return jobs;
 	}
 
-	@GetMapping("/getAllJobs")
-	public List<Job> getAllJobs(){
+	@GetMapping("/getAllJobs") public List<Job> getAllJobs() {
 		List<Job> jobs = null;
 		jobs = JobsDao.getAllJobs();
 		return jobs;
 	}
 
-	@GetMapping("/getAllApplications")
-	public List<JobApplication> getAllApplications(){
+	@GetMapping("/getAllApplications") public List<JobApplication> getAllApplications() {
 		List<JobApplication> applications = null;
 		applications = JobsDao.getAllJobApplications();
 		return applications;
 	}
 
-	@GetMapping("/getAllRecruters")
-	public List<Recruiter> getAllRecruiters(){
-		List<Recruiter>  recruiters = null;
+	@GetMapping("/getAllRecruters") public List<Recruiter> getAllRecruiters() {
+		List<Recruiter> recruiters = null;
 		recruiters = JobsDao.getAllRecruiters();
 		return recruiters;
 	}
 
-	@GetMapping("/getAllJobApplicationsInAppliedStatus")
-	public  List<JobApplication> getAllJobApplicationsInAppliedStatus() {
+	@GetMapping("/getAllJobApplicationsInAppliedStatus") public List<JobApplication> getAllJobApplicationsInAppliedStatus() {
 		List<JobApplication> jobApplications = null;
 		jobApplications = JobsDao.getAllJobApplicationsInAppliedStatus();
 		return jobApplications;
 	}
-	@GetMapping("/getAllJobApplicationsInAppliedStatusByJobId/{jobId}")
-	public  List<JobApplication> getAllJobApplicationsInAppliedStatusByJobId(int jobId) {
+
+	@GetMapping("/getAllJobApplicationsInAppliedStatusByJobId/{jobId}") public List<JobApplication> getAllJobApplicationsInAppliedStatusByJobId(int jobId) {
 		List<JobApplication> jobApplications = null;
 		jobApplications = JobsDao.getAllJobApplicationsInAppliedStatusByJobId(jobId);
 		return jobApplications;
 	}
 
-
-	}
+}
