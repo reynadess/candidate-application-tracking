893752a5095d8fafc1ac6843207e18b7893d9cf4
Sai Chaitanya Raparti <chaitanya.raparti@ezetap.com>
2023-11-25 16:18:49 +0530

    Added Controllers

--- back-end/application-tracking/src/main/java/com/phenom/candidate/applicationtracking/controller/TrackingController.java
+++ back-end/application-tracking/src/main/java/com/phenom/candidate/applicationtracking/controller/TrackingController.java
@@ -3,6 +3,8 @@ package com.phenom.candidate.applicationtracking.controller;
 import com.phenom.candidate.applicationtracking.dao.JobsDao;
 import com.phenom.candidate.applicationtracking.entities.Candidate;
 import com.phenom.candidate.applicationtracking.entities.Job;
+import com.phenom.candidate.applicationtracking.entities.JobApplication;
+import com.phenom.candidate.applicationtracking.entities.Recruiter;
 import com.phenom.candidate.applicationtracking.service.JobRanking;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,4 +23,40 @@ public class TrackingController {
 		List<Job> jobs = JobRanking.getJobsBasedOnCandidateSkills(candidate);
 		return jobs;
 	}
-}
+
+	@GetMapping("/getAllJobs")
+	public List<Job> getAllJobs(){
+		List<Job> jobs = null;
+		jobs = JobsDao.getAllJobs();
+		return jobs;
+	}
+
+	@GetMapping("/getAllApplications")
+	public List<JobApplication> getAllApplications(){
+		List<JobApplication> applications = null;
+		applications = JobsDao.getAllJobApplications();
+		return applications;
+	}
+
+	@GetMapping("/getAllRecruters")
+	public List<Recruiter> getAllRecruiters(){
+		List<Recruiter>  recruiters = null;
+		recruiters = JobsDao.getAllRecruiters();
+		return recruiters;
+	}
+
+	@GetMapping("/getAllJobApplicationsInAppliedStatus")
+	public  List<JobApplication> getAllJobApplicationsInAppliedStatus() {
+		List<JobApplication> jobApplications = null;
+		jobApplications = JobsDao.getAllJobApplicationsInAppliedStatus();
+		return jobApplications;
+	}
+	@GetMapping("/getAllJobApplicationsInAppliedStatusByJobId/{jobId}")
+	public  List<JobApplication> getAllJobApplicationsInAppliedStatusByJobId(int jobId) {
+		List<JobApplication> jobApplications = null;
+		jobApplications = JobsDao.getAllJobApplicationsInAppliedStatusByJobId(jobId);
+		return jobApplications;
+	}
+
+
+	}
